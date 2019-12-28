09619e239989eb3ef8f1bc9d5935c17159196bff
Iványi Béla <ivanyi.beci@gmail.com>
2019-12-28 16:17:25 +0100

    Uses the type alias for the underlying type

--- 2019/day-09/1/main.cpp
+++ 2019/day-09/1/main.cpp
@@ -3,9 +3,10 @@
 
 int main() {
 
+	using value_type = SimpleIntCode::value_type;
 	SimpleIntCode sic{ std::cin };
-	std::vector<long long> input{ 1 };
-	std::vector<long long> output;
+	std::vector<value_type> input{ 1 };
+	std::vector<value_type> output;
 	sic.compute(input, output);
 
 	for (long long i : output) {
