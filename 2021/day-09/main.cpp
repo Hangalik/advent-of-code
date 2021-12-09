41889f765e5baa75fc38627cd49fcaf2434a18c3
Iványi Béla <ivanyi.beci@gmail.com>
2021-12-09 22:02:05 +0100

    Solves day-09 part 2

--- 2021/day-09/main.cpp
+++ 2021/day-09/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
+#include <unordered_set>
+#include <list>
 
 #include "input_selector.h"
 
@@ -43,12 +46,118 @@ int f1(std::istream& in) {
 	return 0;
 }
 
-int f2(std::istream& ) {
+class FloorLevel {
+public:
+	explicit FloorLevel(int level) : level(level) {}
+
+	void addNeighbour(FloorLevel* newNeighbour) {
+		if (neighbours.size() >= 4u) {
+			throw std::length_error("FloorLevel's neighbour count cannot exceed 4");
+		}
+		neighbours.insert(newNeighbour);
+	}
+
+	const std::unordered_set<FloorLevel*>& getNeighbours() const {
+		return neighbours;
+	}
+
+	bool isLowPoint() const {
+		for (FloorLevel* neighbor : neighbours) {
+			if (neighbor->level <= level) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	const int level;
+private:
+	std::unordered_set<FloorLevel*> neighbours;
+};
+
+void linkNeighbours(FloorLevel& lhs, FloorLevel& rhs) {
+	lhs.addNeighbour(&rhs);
+	rhs.addNeighbour(&lhs);
+}
+
+int basinSize(const FloorLevel& floorLevel) {
+	int size = 0;
+	std::unordered_set<const FloorLevel*> visitedFloors;
+	std::unordered_set<const FloorLevel*> activeFloors;
+	activeFloors.insert(&floorLevel);
+	while (!activeFloors.empty()) {
+		auto current = activeFloors.begin();
+		const FloorLevel* currentFloor = *current;
+		visitedFloors.insert(activeFloors.extract(current));
+		size++;
+
+		for (const FloorLevel* neighbour : currentFloor->getNeighbours()) {
+			if (neighbour->level != 9 && !visitedFloors.contains(neighbour)) {
+				activeFloors.insert(neighbour);
+			}
+		}
+	}
+	return size;
+}
+
+int f2(std::istream& in) {
+	// Needs to be a container that doesn't copy around :P
+	std::list<std::list<FloorLevel>> floorLevels;
+	std::string line;
+	while (in >> line) {
+		std::list<FloorLevel> lineLevels;
+		for (size_t i = 0; i < line.size(); i++) {
+			int point = std::stoi(line.substr(i, 1));
+			lineLevels.emplace_back(point);
+
+			if (i > 0u) {
+				auto lastIt = lineLevels.rbegin();
+				FloorLevel& last = *lastIt;
+				lastIt++;
+				FloorLevel& beforeLast = *lastIt;
+				linkNeighbours(last, beforeLast);
+			}
+			if (!floorLevels.empty()) {
+				auto lastFloor = floorLevels.back().begin();
+				std::advance(lastFloor, i);
+				linkNeighbours(lineLevels.back(), *lastFloor);
+			}
+		}
+		floorLevels.push_back(std::move(lineLevels));
+	}
+
+	std::multiset<int, std::less<int>> basinSizes;
+	for (const std::list<FloorLevel>& floors : floorLevels) {
+		for (const FloorLevel& level : floors) {
+			if (level.isLowPoint()) {
+				int sizeOfBasin = basinSize(level);
+				//std::cout << "Low point basin size: " << sizeOfBasin << std::endl;
+				basinSizes.insert(sizeOfBasin);
+			}
+		}
+	}
+
+	if (basinSizes.size() < 3u) {
+		std::cerr << "There is no 3 basins in the input" << std::endl;
+		return 1;
+	}
+	auto bigEnd = basinSizes.rbegin();
+	int most = *bigEnd;
+	bigEnd++;
+	int secondMost = *bigEnd;
+	bigEnd++;
+	int thirdMost = *bigEnd;
+	std::cout << most << " * " << secondMost << " * " << thirdMost << " = " << most * secondMost * thirdMost << std::endl;
 
 	return 0;
 }
 
 int main(int argc, const char* argv[]) {
 
-	return runWithProperInput(argc, argv, f1, f2);
+	try {
+		return runWithProperInput(argc, argv, f1, f2);
+	} catch (std::logic_error& le) {
+		std::cerr << le.what() << std::endl;
+		return 1;
+	}
 }
